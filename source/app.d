d2858712250a184447094fa24cace0e3ea197e53
Clipsey <TheisCHR@live.dk>
2018-05-22 20:58:27 +0200

    audio compilation (ogg only)

--- source/app.d
+++ source/app.d
@@ -4,40 +4,56 @@ import ppc.utils;
 
 import std.random;
 import std.algorithm.searching;
-
 import std.conv;
-
 import std.path;
+import std.file;
 
-void main(string[] args)
+import sdlang;
+import cfile;
+
+void main(string[] args_o)
 {
-	if (args.length > 1) {
-		if (args[1] == "--convert" || args[1] == "-c") {
-			for (int i = 2; i < args.length; i++) {
-				//Setup base factories.
-				SetupBaseFactories();
-				
-				if (args[i].endsWith(".png") || args[i].endsWith(".jpeg") || args[i].endsWith(".jpg") || args[i].endsWith(".bmp")) {
-					ConvertImage(args[i]);
-				} else {
-					writeln("<Warning> unknown filetype...");
-					ConvertRaw(args[i]);
-				}
+	string[] args = args_o[1..$];
+	try {
+		if (args.length < 2) {
+			if (args.length == 0) {
+				HandleProjectFile("content.sdl");
+			} else {
+				HandleProjectFile(args[0]);
 			}
-			writeln("Done.");
-			return;
 		} else {
-			writeln("Invalid parameters!
-			
-Usage:
-ppcc --convert (input file) (output file name (WITHOUT .ppc extension)) (etc.)");
+			if (args[0] == "--convert" || args[0] == "-c") {
+				for (int i = 1; i < args.length; i++) {
+					//Setup base factories.
+					SetupBaseFactories();
+					
+					if (args[i].endsWith(".png") || args[i].endsWith(".jpeg") || args[i].endsWith(".jpg") || args[i].endsWith(".bmp")) {
+						ConvertImage(args[i]);
+					} else if (args[i].endsWith(".ogg")) {
+						ConvertAudio(args[i]);
+					} else {
+						writeln("<Warning> unknown filetype...");
+						ConvertRaw(args[i]);
+					}
+				}
+				writeln("Done.");
+				return;
+			} else {
+				writeln("Invalid parameters!
+				
+	Usage:
+	ppcc --convert (input file) (output file name (WITHOUT .ppc extension)) (etc.)");
+			}
 		}
-	} else {
-		writeln("Not enough arguments!
-		
-Usage:
-ppcc --convert (input file) (output file name (WITHOUT .ppc extension)) (etc.)");
+	} catch (Exception ex) {
+		writeln("Failed building PPCC project!\nReason:\n"~ex.message);
 	}
+}
+
+void HandleProjectFile(string file) {
+	if (!exists(file)) throw new Exception("Project file " ~ file ~ " was not found!");
+	ContentFileDefinition def = new ContentFileDefinition(parseSource(file));
+
 }
 
 void ConvertImage(string file) {
@@ -50,8 +66,22 @@ void ConvertImage(string file) {
 		data = Combine(data, buff);
 	}
 	f.close();
-	img.Convert(data);
+	img.Convert(data, 0);
 	WriteContentFile(img, file);
+}
+
+void ConvertAudio(string file) {
+	string name = stripExtension(file);
+	Audio aud = new Audio(name);
+	File f = File(file);
+	ubyte[] data = [];
+	foreach(ubyte[] buff; f.byChunk(4096)) {
+		data = Combine(data, buff);
+	}
+	f.close();
+	aud.Convert(data, AudioStorageType.OGG);
+	writeln("A");
+	WriteContentFile(aud, file);
 }
 
 void ConvertRaw(string file) {
@@ -64,7 +94,7 @@ void ConvertRaw(string file) {
 		data = Combine(data, buff);
 	}
 	f.close();
-	raw.Convert(data);
+	raw.Convert(data, 0);
 	WriteContentFile(raw, file);
 }
 
